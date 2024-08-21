8aba5b1d2fe967390a0d424db02ad57c419857a5
JiIJu <eldk313131@gmail.com>
2024-08-21 19:04:14 +0900

    quat2eul

--- ranger_control/src/ranger_controller.cpp
+++ ranger_control/src/ranger_controller.cpp
@@ -1,11 +1,16 @@
 #include "ranger_control/ranger_controller.h"
 
 ranger_controller::ranger_controller(ros::NodeHandle &nh) : nh_(nh) {
-    get_topic();  
+    get_topic();
+    get_frame();
 }
 
 ranger_controller::~ranger_controller() {
     
+}
+
+void ranger_controller::get_frame(){
+
 }
 
 
@@ -15,6 +20,7 @@ void ranger_controller::get_topic() {
     waypathR_sub = nh_.subscribe("waypathR", 10, &ranger_controller::waypathR_callback, this);
     rrtR_sub = nh_.subscribe("rrtR", 10, &ranger_controller::rrtR_callback, this);
     final_path_sub = nh_.subscribe("final_path", 10, &ranger_controller::final_path_callback, this);
+    odometry_sub = nh_.subscribe("odometry", 10, &ranger_controller::odometry_callback, this);
 }
 
 void ranger_controller::amcl_pose_stamp_callback(const geometry_msgs::PoseStamped::ConstPtr& amcl_pose_stamp_msg) {
@@ -34,4 +40,25 @@ void ranger_controller::rrtR_callback(const geometry_msgs::PoseStamped::ConstPtr
 
 void ranger_controller::final_path_callback(const std_msgs::Int32::ConstPtr& final_path_msg){
     check = final_path_msg->data;
+}
+
+void ranger_controller::odometry_callback(const nav_msgs::Odometry::ConstPtr &odometry_msg){
+    tf::Quaternion quat(
+        odometry_msg->pose.pose.orientation.x,
+        odometry_msg->pose.pose.orientation.y,
+        odometry_msg->pose.pose.orientation.z,
+        odometry_msg->pose.pose.orientation.w
+
+    );
+
+    double roll, pitch, yaw;
+    quaternionToEuler(quat, roll, pitch, yaw);
+
+  
+    yaw_raw = yaw;
+}
+
+void ranger_controller::quaternionToEuler(const tf::Quaternion& quat, double& roll, double& pitch, double& yaw) {
+    tf::Matrix3x3 m(quat);
+    m.getRPY(roll, pitch, yaw);
 }
