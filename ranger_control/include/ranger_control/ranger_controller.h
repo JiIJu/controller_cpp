038a30c157f7d5d3c783de4a191407c85c31b9d2
JiIJu <JiIJu@github.com>
2024-08-30 14:54:14 +0900

    60% complete..?

--- ranger_control/include/ranger_control/ranger_controller.h
+++ ranger_control/include/ranger_control/ranger_controller.h
@@ -1,17 +1,102 @@
 #ifndef RANGER_CONTROLLER_H
 #define RANGER_CONTROLLER_H
 
+#define _USE_MATH_DEFINES
+
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Int32.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/tf.h>
-#include<math.h>
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
+#include <vector>  // 벡터 사용을 위한 헤더 추가
+#include <cmath>
+
+
+class DiscreteTimeIntegrator {
+private:
+    double Ts;              // 샘플링 시간
+    double previousOutput;  // 이전 출력 값 (적분 값)
+    double gain;            // 이득
+
+public:
+    // 새로운 생성자: 이득, 초기 조건, 샘플링 시간 초기화
+    DiscreteTimeIntegrator(double gainValue, double initialCondition, double samplingTime) 
+        : gain(gainValue), previousOutput(initialCondition), Ts(samplingTime) {}
+
+    // 적분 계산 메서드
+    double calculate(double currentInput) {
+        // 이산 적분 계산: y[k] = y[k-1] + Ts * gain * u[k]
+        double output = previousOutput + Ts * gain * currentInput;
+
+        // 출력 값을 저장하여 다음 스텝에서 사용
+        previousOutput = output;
+
+        return output;
+    }
+
+    // 출력 값 초기화
+    void reset() {
+        previousOutput = 0.0;
+    }
+};
+
+// 이산 미분 및 전달 함수 클래스 정의
+class DiscreteDerivative {
+private:
+    double previousInput = 0.0;
+    double Ts;
+
+public:
+    DiscreteDerivative(double samplingTime) : Ts(samplingTime) {}
+
+    double calculate(double currentInput) {
+        double derivative = (currentInput - previousInput) / Ts;
+        previousInput = currentInput;
+        return derivative;
+    }
+};
+
+class DiscreteTransferFunction {
+private:
+    std::vector<double> numerator;
+    std::vector<double> denominator;
+    std::vector<double> inputHistory;
+    std::vector<double> outputHistory;
+
+public:
+    DiscreteTransferFunction(const std::vector<double>& num, const std::vector<double>& den)
+        : numerator(num), denominator(den) {
+        inputHistory.resize(num.size(), 0.0);
+        outputHistory.resize(den.size(), 0.0);
+    }
+
+    double calculate(double currentInput) {
+        inputHistory.insert(inputHistory.begin(), currentInput);
+        inputHistory.pop_back();
+
+        double output = 0.0;
+
+        for (size_t i = 0; i < numerator.size(); ++i) {
+            output += numerator[i] * inputHistory[i];
+        }
+
+        for (size_t i = 1; i < denominator.size(); ++i) {
+            output -= denominator[i] * outputHistory[i];
+        }
+
+        output /= denominator[0];
+        outputHistory.insert(outputHistory.begin(), output);
+        outputHistory.pop_back();
+
+        return output;
+    }
+};
+
 class ranger_controller {
 public:
-    
     ranger_controller(ros::NodeHandle &nh);
     virtual ~ranger_controller();
 
@@ -25,7 +110,8 @@ private:
     ros::Subscriber final_path_sub;
     ros::Subscriber odometry_sub;
 
-    // gain 값
+    ros::Publisher cmd_vel_pub;
+
     double vr;
     double g1;
     double g2;
@@ -33,7 +119,6 @@ private:
     double L1;
     double L2;
 
-    // 토픽 변수
     double x;
     double y;
     double xr;
@@ -42,7 +127,6 @@ private:
     double rrt_yr;
     int check;
 
-    // 프레임 변수
     double ex;
     double ey;
     double exb;
@@ -53,21 +137,40 @@ private:
     double quaternion[4];  
     double referen_x;
     double referen_y;
+    double yaw_d_ref;
+    double yaw_d;
+    double yaw_rate_d;
+    // double d_hat;
+    double temp1;
+    double temp2;
+    double Kp;
+    double K;
+    double Vr;
+    double u2;
+    double e_yaw;
 
-
-    // matlab function 변수
     static int cnt_sum;
     static double theta_old;
+    static double cnt_old;
+    static double d_hat;
+    static double eyb_hat;
+    static double w;
     double sum_theta;
+
+    static int cnt_sum2;
+    double sum_theta2;
 
     static double previous_u;
     static double previous_yaw_rate;
 
     ros::Duration dt;
 
+    DiscreteDerivative derivativeBlock;  // 이산 미분 블록
+    DiscreteTransferFunction transferFunctionBlock;  // 이산 전달 함수 블록
+    DiscreteTimeIntegrator integratorBlock;
+
     void get_topic();
     void get_frame();
-
 
     void amcl_pose_stamp_callback(const geometry_msgs::PoseStamped::ConstPtr& amcl_pose_stamp_msg);
     void waypathR_callback(const geometry_msgs::PoseStamped::ConstPtr& waypathR_msg);
@@ -78,7 +181,10 @@ private:
 
     void processAngle(double theta, double& sum_theta, int& cnt_sum);
     void pcheck(int u, double& way_x , double& way_y, double& rrt_x, double& rrt_y);
-};
+    void yawd_cal(double ex, double& ey);
+    void vehicle_yaw_func(double theta, double& sum_theta, int& cnt_sum);\
 
+    void controller();
+};
 
 #endif
--- ranger_control/src/ranger_controller.cpp
+++ ranger_control/src/ranger_controller.cpp
@@ -1,20 +1,92 @@
 #include "ranger_control/ranger_controller.h"
 
 int ranger_controller::cnt_sum = 0;
+int ranger_controller::cnt_sum2 = 0;
 double ranger_controller::theta_old = 0.0;
+double ranger_controller::cnt_old = 0.0;
 
 double ranger_controller::previous_u = 0.0;
 double ranger_controller::previous_yaw_rate = 0.0;
+double ranger_controller::d_hat = 1.0;
+double ranger_controller::eyb_hat = 1.0;
+double ranger_controller::w = 1.0;
 
-ranger_controller::ranger_controller(ros::NodeHandle &nh) : nh_(nh) {
+
+
+// 이산 전달 함수와 미분의 계수 설정
+std::vector<double> lpfNumerator = {0.0653};  // 전달 함수 분자 계수
+std::vector<double> lpfDenominator = {1.0, -0.9347};  // 전달 함수 분모 계수
+
+// Integrator 초기값 설정
+double integratorGain = 1.0;  // 이득 값
+double integratorInitialCondition = 0.0;  // 초기 조건
+double integratorSampleTime = -1;  // 샘플링 시간 (-1은 불규칙 샘플링을 의미)
+
+ranger_controller::ranger_controller(ros::NodeHandle &nh) 
+    : nh_(nh), 
+      derivativeBlock(dt.toSec()),  // 이산 미분 샘플링 시간 설정
+      transferFunctionBlock(lpfNumerator, lpfDenominator),// 전달 함수 계수 초기화 
+      integratorBlock(integratorGain, integratorInitialCondition, integratorSampleTime)
+      {  
 
     L1 = 1.0;
     L2 = 1.0;
-
+    
     get_topic();
+
+    cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/four_wheel_steering_controller/cmd_vel", 10);
+
 }
 
 ranger_controller::~ranger_controller() {
+}
+
+void ranger_controller::controller(){
+    double L = fabs(exb);
+    if(L>20.0){
+        L = 20.0;
+    }
+    else if(L<0.5){
+        L = 0.5;
+    }
+
+    temp1 = L * w + d_hat;
+
+    eyb_hat = integratorBlock.calculate(temp1);
+    temp1 = (1-exp(-Kp * fabs(eyb))) * K;
+    if(eyb>0){
+        u2 = temp1 * 1;
+    }
+    else if(eyb<0) {
+        u2 = temp1 * -1;
+    }
+    else{
+        u2 = temp1 * 0;
+    }
+    ROS_INFO("u2 : %f" , u2);
+    w = u2;
+
+        geometry_msgs::Twist cmd_vel_msg;
+    if(fabs(u2) < fabs(0.0001)){
+        cmd_vel_msg.linear.x = 0.0; 
+        cmd_vel_msg.linear.y = 0.0; 
+        cmd_vel_msg.linear.z = 0.0; 
+
+        cmd_vel_msg.angular.x = 0.0;
+        cmd_vel_msg.angular.y = 0.0;
+        cmd_vel_msg.angular.z = 0.0;
+        }
+    else{
+        cmd_vel_msg.linear.x = 0.5; 
+        cmd_vel_msg.linear.y = 0.0; 
+        cmd_vel_msg.linear.z = 0.0; 
+
+        cmd_vel_msg.angular.x = 0.0;
+        cmd_vel_msg.angular.y = 0.0;
+        cmd_vel_msg.angular.z = w/L;
+    }     
+    ROS_INFO("w/L : %f" , w/L);
+    cmd_vel_pub.publish(cmd_vel_msg);
 
 }
 
@@ -24,13 +96,11 @@ void ranger_controller::get_frame(){
     ey = y - referen_y;
 
     exb = cos(yaw) * ex + ey * sin(yaw);
-    eyb = -sin(yaw) * ex + ey * cos(yaw);
+    eyb = -sin(yaw) * ex + ey * cos(yaw); 
 
-    ROS_INFO("exb : %f , eyb : %f" , exb,eyb);
+    e_yaw = atan2(-eyb,-exb);
 }
 
-
-// 토픽 subscribe
 void ranger_controller::get_topic() {
     amcl_pose_stamp_sub = nh_.subscribe("amcl_pose_stamp", 10, &ranger_controller::amcl_pose_stamp_callback, this);
     waypathR_sub = nh_.subscribe("waypathR", 10, &ranger_controller::waypathR_callback, this);
@@ -74,27 +144,24 @@ void ranger_controller::odometry_callback(const nav_msgs::Odometry::ConstPtr &od
     processAngle(yaw_raw, sum_theta, cnt_sum);
   
     double dt_sec = dt.toSec();
-
-    double K = 1.0;   
-    // double Ts = 0.01;
-
     double u = yaw; 
-    double derivative = K * (u - previous_u) / dt_sec;
 
+    // 이산 미분 계산
+    double derivative = derivativeBlock.calculate(u);  
+    // 이산 전달 함수 계산
+    yaw_rate = transferFunctionBlock.calculate(derivative);
+    // ROS_INFO("previous_yaw_rate : %f" , previous_yaw_rate);
+    // ROS_INFO("yaw_rate : %f" , yaw_rate);
     previous_u = u; 
-
-    // 이 부분 확인 필요함
-    
-    // LPF3: c2d(7/(s+7), 0.01)
-    double LPF3_numerator = 0.0653;
-    double LPF3_denominator = -0.9347;
-
-    yaw_rate = LPF3_numerator * derivative + LPF3_denominator * previous_yaw_rate;
-
     previous_yaw_rate = yaw_rate; 
 
     get_frame();
 
+    yawd_cal(ex,ey);
+
+    vehicle_yaw_func(yaw_d_ref, sum_theta2, cnt_sum2);
+
+    controller();
 
 
 }
@@ -114,9 +181,12 @@ void ranger_controller::processAngle(double theta, double& sum_theta, int& cnt_s
     }
 
     if (dire == 1) {
-        cnt_sum += 1;
+        cnt_sum = cnt_old+ 1;
     } else if (dire == -1) {
-        cnt_sum -= 1;
+        cnt_sum = cnt_old - 1;
+    }
+    else {
+        cnt_sum = cnt_old;
     }
 
     if (cnt_sum == 0) {
@@ -127,11 +197,10 @@ void ranger_controller::processAngle(double theta, double& sum_theta, int& cnt_s
     yaw = sum_theta;
 
     theta_old = theta;
-    
+    cnt_old = cnt_sum;
 }
 
 void ranger_controller::pcheck(int u, double& way_x , double& way_y, double& rrt_x, double& rrt_y) {
-    
     if(u==0){
         referen_x = rrt_x;
         referen_y = rrt_y;
@@ -153,7 +222,58 @@ void ranger_controller::pcheck(int u, double& way_x , double& way_y, double& rrt
         g1 = 0;
         g2 = 0;
     }
-    
+    Kp = g1;
+    K = g2;
+    Vr = vr;
+}
+
+void ranger_controller::yawd_cal(double ex, double& ey) {
+    if(ex==0 && ey>=0){
+        yaw_d_ref = M_PI / 2;
+    }
+    else if(ex == 0 && ey <0){
+        yaw_d_ref = -M_PI / 2;
+    }
+    else{
+        yaw_d_ref = atan2(-ey , -ex);
+    }
+}
+
+void ranger_controller::vehicle_yaw_func(double theta, double& sum_theta2, int& cnt_sum2) {
+    int dire=0;
+
+    if (theta - theta_old < -6.1) {
+        dire = 1;
+    } else if (theta - theta_old > 6.1) {
+        dire = -1;
+    }
+
+    if (dire == 1) {
+        cnt_sum2 = cnt_old+ 1;
+    } else if (dire == -1) {
+        cnt_sum2 = cnt_old - 1;
+    }
+    else {
+        cnt_sum2 = cnt_old;
+    }
+
+    if (cnt_sum2 == 0) {
+        sum_theta2 = theta;
+    } else {
+        sum_theta2 = theta + cnt_sum2 * 2 * M_PI;
+    }
+
+    theta_old = theta;
+    cnt_old = cnt_sum2;
+  
+    // 이산 전달 함수 계산
+    yaw_d = transferFunctionBlock.calculate(sum_theta2);
+
+    // 이산 미분 계산
+    double derivative = derivativeBlock.calculate(sum_theta2);  
+    // 이산 전달 함수 계산
+    yaw_rate_d = transferFunctionBlock.calculate(derivative);
+    // ROS_INFO("yaw_rate_d : %f" , yaw_rate_d);
 }
 
 void ranger_controller::updateDeltaTime(const ros::Duration& dt_) {
